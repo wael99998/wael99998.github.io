5b2aecc583831fe36559fc98d33391d97d352e46
wael99998 <ziyaeddein@gmail.com>
2026-09-20 13:16:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-self.addEventListener('fetch', function(e) {});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11845804
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
